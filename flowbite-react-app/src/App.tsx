6277fd2f7b913526d3171be30c56ba155e849909
Shivendra Tripathi <shivendraking19@gmail.com>
2025-04-06 07:17:07 +0530

    first commit

---
+++ flowbite-react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { DarkThemeToggle } from "flowbite-react";
+
+export default function App() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24 dark:bg-gray-900">
+      Hello
+    </main>
+  );
+}
